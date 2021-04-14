a2a84d01b40ebf41405cda196db1c4c041512e1c
ANN <chistyakova_69@mail.ru>
2021-04-14 22:52:09 +0300

    correct 11

--- Task_11/src/appline/com/Main.java
+++ Task_11/src/appline/com/Main.java
@@ -8,16 +8,15 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String first_number = in.next();
         System.out.println("Введите второе число");
-        int second_number = in.nextInt();
-        int first_number_int = Integer.parseInt(first_number);
-        if(first_number_int>second_number){
-            System.out.println("Большее число: "+first_number_int);
+        int  second_number = (int) in.nextDouble();
+ //       int first_number_int = Integer.parseInt(first_number);
+        double first_number_double=Double.parseDouble(first_number);
+        if(first_number_double>second_number){
+            System.out.println("Большее число: "+first_number_double);
             System.out.println("Меньшее число: "+(double)second_number);
-
-        }else if(first_number_int<second_number){
+        }else if(first_number_double<second_number){
             System.out.println("Большее число: "+second_number);
-            System.out.println("Меньшее число: "+(double)first_number_int);
-
+            System.out.println("Меньшее число: "+(double)first_number_double);
         }else{System.out.println("Значения равны: "+(double)second_number);}
     }
 
